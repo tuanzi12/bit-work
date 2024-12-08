6b0db28c767b3c9934040f7f6fc91bbb15a38693
tuanzi712 <1967179366@qq.com>
2024-12-08 15:31:42 +0800

    双链表增删查改

---
+++ List(双链表增删查改)/23.08.02_3(双链表增删查改)/Test.c
@@ -0,0 +1,33 @@
+#include "List.h"
+void testlist1()
+{
+	ListNode* plist = ListNodeInit();
+	ListPushBack(plist , 1);
+	ListPushBack(plist, 2);
+	ListPushBack(plist, 3);
+	ListPrint(plist);
+	printf("\n");
+	ListPopBack(plist);
+	ListPrint(plist);
+
+	ListDestory(plist);
+	plist = NULL;
+}
+
+void testlist2()
+{
+	ListNode* plist = ListNodeInit();
+	ListPushFront(plist, 1);
+	ListPushFront(plist, 2);
+	ListPushFront(plist, 3);
+	ListPrint(plist);
+	printf("\n");
+	ListPopFront(plist);
+	ListPrint(plist);
+}
+
+int main()
+{
+	testlist1();
+	return 0;
+}
